b4ae083b24259edf5b8ab296214fb7b3725d67da
Gilbert Lau <gmflau@gmail.com>
2018-03-29 23:22:22 -0700

    Added missing "

--- post_network/userdata/lcm_opscenter_userdata.sh
+++ post_network/userdata/lcm_opscenter_userdata.sh
@@ -2,7 +2,7 @@
 
 cd ~opc
 
-release=vcn_peering"
+release="vcn_peering"
 curl https://raw.githubusercontent.com/DSPN/oracle-bmc-terraform-dse/$release/post_network/userdata/lcm_opscenter.sh > lcm_opscenter.sh
 
 chmod +x lcm_opscenter.sh
